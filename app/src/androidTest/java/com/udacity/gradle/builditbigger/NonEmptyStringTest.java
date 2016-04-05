62ab4d567ea480169e18a6368db2469d4416f32d
F3838284 <nkosinathi.nkosi@fnb.co.za>
2016-04-05 10:33:49 +0200

    Corrections adding a test class

---
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/NonEmptyStringTest.java
@@ -0,0 +1,30 @@
+package com.udacity.gradle.builditbigger;
+
+import android.test.AndroidTestCase;
+import android.util.Log;
+
+/**
+ * Created by F3838284 on 4/5/2016.
+ */
+public class NonEmptyStringTest extends AndroidTestCase {
+
+    private static final String LOG_TAG = "NonEmptyStringTest";
+
+    @SuppressWarnings("unchecked")
+    public void test() {
+
+        // Testing that Async task successfully retrieves a non-empty string
+        // You can test this from androidTest -> Run 'All Tests'
+        Log.v("NonEmptyStringTest", "Running NonEmptyStringTest test");
+        String result = null;
+        EndpointsAsyncTask endpointsAsyncTask = new EndpointsAsyncTask(getContext(), null);
+        endpointsAsyncTask.execute();
+        try {
+            result = endpointsAsyncTask.get();
+            Log.d(LOG_TAG, "Retrieved a non-empty string successfully: " + result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        assertNotNull(result);
+    }
+}
